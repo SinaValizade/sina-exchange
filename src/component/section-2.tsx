571b15b32cfe0585991f4d06546b9395a8fff05b
Your Name <sinasoft2000@gmail.com>
2024-08-24 19:41:23 +0330

    update :radioactive:

--- src/component/section-2.tsx
+++ src/component/section-2.tsx
@@ -1,24 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import axios, { Axios } from 'axios';
-// import { SetCryptoPrice } from '../store';
-import { useDispatch, UseDispatch , useSelector } from 'react-redux';
+import React, { useEffect} from 'react';
+import axios from 'axios';
+import {SetCryptoImg, SetCryptoPrice} from '../store';
+import { useDispatch, useSelector } from 'react-redux';
 import { Slide21 } from './slide2-1';
+import btcLogo from "../files/cryptos/bitcoin.png";
+import ethLogo from "../files/cryptos/ETH.png.png";
+import xrpLogo from "../files/cryptos/XRP.png.png";
+import adaLogo from "../files/cryptos/cardano.png";
+import ltcLogo from "../files/cryptos/litecoin.png";
+import dogeLogo from "../files/cryptos/dogecoin.png";
 
 
 export const Section2 = () => {
     // @ts-ignore
-    // const [cryptoPrice, setCryptoPrice] = useState([]);
     const dispatch = useDispatch()
-    const selector = useSelector((state:any) => state.sinaExcgange)
+    const selector = useSelector((state:any) => state.sinaExchange)
+
+
 
     useEffect(() => {
-        axios.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH,BTC,ADA,TRX,DOGE&tsyms=USD&api_key=81c0920a80c2c24418818bb137ffee40eb4e52bd959b2ce957d164b64a0f1cf8").then((res) => {
+        axios.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,XRP,BTC,ADA,LTC,DOGE&tsyms=USD&api_key=81c0920a80c2c24418818bb137ffee40eb4e52bd959b2ce957d164b64a0f1cf8").then((res) => {
             // setCryptoPrice(res.data)
+            dispatch(SetCryptoPrice({price: res.data}));
         })
     },[])
 
+    console.log(selector.cryptoPrice)
+
     // @ts-ignore
-    
     return (
         <>
             <section className="bg-blue-600 h-full">
--- src/store.tsx
+++ src/store.tsx
@@ -1,17 +1,23 @@
 //@ts-ignore
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import {configureStore, createSlice} from '@reduxjs/toolkit';
 
 const userSlice = createSlice({
     name: "sinaExcgange",
-    initialState: {cryptoPrice:[]},
+    initialState: {
+        cryptoPrice: [],
+        cryptoImg: []
+    },
     reducers: {
-        SetCryptoPrice : (state , action) => {
+        SetCryptoPrice: (state, action) => {
             state.cryptoPrice = action.payload.price
+        },
+        SetCryptoImg: (state, action) => {
+            state.cryptoImg = action.payload.img
         }
     }
 })
 
-// export const {SetCryptoPrice} = userSlice.actions
+export const {SetCryptoPrice,SetCryptoImg} = userSlice.actions
 export const store = configureStore({
     reducer: {
         sinaExchange: userSlice.reducer
